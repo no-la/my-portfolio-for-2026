ace7f09d486edd5ab106ce81c9aac015dafaf81b
no-la <nzyc0216@icloud.com>
2024-09-18 00:25:45 +0900

    add my-others

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -5,6 +5,7 @@ import MyHeader from './my-header/my-header';
 import MyProfile from './my-profile/my-profile';
 import MySkills from './my-skills/my-skills';
 import MyProjects from './my-projects/my-projects';
+import MyOthers from './my-others/my-others';
 
 const App = () => {
   return (
@@ -13,6 +14,7 @@ const App = () => {
       <MyProfile />
       <MySkills />
       <MyProjects />
+      <MyOthers />
     </>
   );
 };
---
+++ src/app/my-others/my-others.jsx
@@ -0,0 +1,12 @@
+import SectionHeader from '../../components/section-header/section-header';
+import './my-others.css';
+
+const MyOthers = () => {
+  return (
+    <section id="others">
+      <SectionHeader>Others</SectionHeader>
+    </section>
+  );
+};
+
+export default MyOthers;
