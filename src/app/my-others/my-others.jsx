c6c86a7018dac221decb0c21551141e812cb9523
no-la <nzyc0216@icloud.com>
2024-09-22 22:28:58 +0900

    add other-card

--- src/app/my-others/my-others.jsx
+++ src/app/my-others/my-others.jsx
@@ -1,13 +1,52 @@
 /** @jsxImportSource @emotion/react */
+import { css } from '@emotion/react';
 import SectionHeader from '../../components/section-header/section-header';
+import OtherCard from './other-card';
+
+const datas = [
+  {
+    name: 'AtCoder',
+    tags: [0, 100],
+    text: '友人に誘われて、2023/9頃に始め、現在も趣味として続けている。現在は緑コーダーで、水色コーダーを目標にしている。',
+  },
+  {
+    name: 'AtCoder',
+    tags: [0, 100],
+    text: '友人に誘われて、2023/9頃に始め、現在も趣味として続けている。現在は緑コーダーで、水色コーダーを目標にしている。',
+  },
+  {
+    name: 'AtCoder',
+    tags: [0, 100],
+    text: '友人に誘われて、2023/9頃に始め、現在も趣味として続けている。現在は緑コーダーで、水色コーダーを目標にしている。',
+  },
+  {
+    name: 'AtCoder',
+    tags: [0, 100],
+    text: '友人に誘われて、2023/9頃に始め、現在も趣味として続けている。現在は緑コーダーで、水色コーダーを目標にしている。',
+  },
+];
+
+const WrapStyle = css`
+  display: flex;
+  flex-wrap: wrap;
+  align-items: center;
+  justify-content: center;
+  padding: 30px 100px;
+  gap: 30px;
+`;
 
 const MyOthers = () => {
   return (
-    <section
-      id="others"
-      css={(theme) => ({ height: '600px' })}
-    >
+    <section id="others">
       <SectionHeader>Others</SectionHeader>
+      <div css={WrapStyle}>
+        {datas.map((data, i) => (
+          <OtherCard
+            key={`other-card-${data.name}`}
+            {...data}
+          ></OtherCard>
+        ))}
+      </div>
     </section>
   );
 };
---
+++ src/app/my-others/other-card.jsx
@@ -0,0 +1,24 @@
+/** @jsxImportSource @emotion/react */
+
+import { css } from '@emotion/react';
+import CardHeader from '../../components/card-header/card-header';
+import TagList from '../../components/tag-list/tag-list';
+
+const wrapStyle = css`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 350px;
+`;
+
+const OtherCard = ({ name, tags, text }) => {
+  return (
+    <div css={wrapStyle}>
+      <CardHeader>{name}</CardHeader>
+      <TagList tags={tags}></TagList>
+      <div>{text}</div>
+    </div>
+  );
+};
+
+export default OtherCard;
