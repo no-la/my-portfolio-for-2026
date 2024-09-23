c10b2283cc9925fa300c8402b2ed1e550030c239
no-la <nzyc0216@icloud.com>
2024-09-23 21:23:56 +0900

    add profile content

--- src/app/my-profile/my-profile.jsx
+++ src/app/my-profile/my-profile.jsx
@@ -1,16 +1,45 @@
 /** @jsxImportSource @emotion/react */
 
+import { css } from '@emotion/react';
+import SectionHeader from '../../components/section-header/section-header';
+import CardHeader from '../../components/card-header/card-header';
+
+const wrapStyle = css`
+  display: flex;
+  padding: 30px 100px;
+  align-items: center;
+  justify-content: center;
+  flex-wrap: wrap;
+  gap: 20px 30px;
+`;
+
 const MyProfile = () => {
   return (
-    <section
-      id="profile"
-      css={(theme) => ({
-        position: 'relative',
-        height: '600px',
-        backgroundColor: theme.colors.secondary,
-      })}
-    >
-      <div>Profile</div>
+    <section>
+      <SectionHeader>Profile</SectionHeader>
+      <div css={wrapStyle}>
+        <div>
+          <img
+            src="src/assets/sample/images/tiwahu.png"
+            alt=""
+            css={{ width: '200px', height: '200px' }}
+          />
+        </div>
+        <div>
+          <CardHeader>のーら</CardHeader>
+          <div>
+            <p>
+              あいうえおあいうえおあいうえおあいうえおあいうえおあいうえお
+            </p>
+            <p>
+              あいうえおあいうえおあいうえおあいうえおあいうえおあいうえお
+            </p>
+            <p>
+              あいうえおあいうえおあいうえおあいうえおあいうえおあいうえお
+            </p>
+          </div>
+        </div>
+      </div>
     </section>
   );
 };
