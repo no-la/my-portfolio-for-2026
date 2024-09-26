700aa8cca83039c1ccdf40a3829eeff9d3e60a5c
no-la <nzyc0216@icloud.com>
2024-09-26 22:00:56 +0900

    change my-footer

--- src/app/my-footer/my-footer.jsx
+++ src/app/my-footer/my-footer.jsx
@@ -2,13 +2,14 @@
 
 import { css } from '@emotion/react';
 
-const height = '50px';
+const height = '60px';
 
 const MyFooter = () => {
   return (
     <footer
       css={(theme) => css`
-        /* height: ${height}; */
+        height: ${height};
+        color: ${theme.colors.white};
         line-height: ${height};
         background-color: ${theme.colors.primary};
         text-align: center;
