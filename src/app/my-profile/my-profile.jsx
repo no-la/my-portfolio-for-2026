eb86ef9821cf18700b27a1cc30d3854dc54bbc6e
no-la <nzyc0216@icloud.com>
2024-09-29 11:08:36 +0900

    modify profile icon

--- src/app/my-profile/my-profile.jsx
+++ src/app/my-profile/my-profile.jsx
@@ -5,13 +5,23 @@ import SectionHeader from '../../components/section-header/section-header';
 import CardHeader from '../../components/card-header/card-header';
 import datas from '../../assets/data/my-profile.json';
 
+const imgSize = '160px';
 const wrapStyle = css`
   display: flex;
   padding: 30px 100px;
   align-items: center;
   justify-content: center;
   flex-wrap: wrap;
-  gap: 20px 30px;
+  gap: 20px 36px;
+`;
+const imgStyle = (theme) => css`
+  width: ${imgSize};
+  height: ${imgSize};
+  /* padding: 12px; */
+  border: 2px solid ${theme.colors.primary};
+  /* border: 1px solid ${theme.colors.black}; */
+  border-radius: 100px;
+  background-color: ${theme.colors.white};
 `;
 const resetPStyle = css`
   margin-bottom: 0;
@@ -23,14 +33,7 @@ const MyProfile = () => {
       <SectionHeader>Profile</SectionHeader>
       <div css={wrapStyle}>
         <div>
-          <img
-            src={datas.image}
-            alt=""
-            css={css`
-              width: 200px;
-              height: 200px;
-            `}
-          />
+          <img src={datas.image} alt="" css={imgStyle} />
         </div>
         <div>
           <CardHeader>{datas.name}</CardHeader>
