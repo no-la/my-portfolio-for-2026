bcf500e87a23448586fcc3a8c8fbe8346829e6d5
no-la <nzyc0216@icloud.com>
2024-09-29 23:35:43 +0900

    change hero-image

--- src/app/my-hero-image/my-hero-image.jsx
+++ src/app/my-hero-image/my-hero-image.jsx
@@ -7,7 +7,40 @@ const MyHeroImage = () => {
     <HeroImage
       imgPath="images/hero-image.jpg"
       id="top"
-    ></HeroImage>
+      wrapStyle={css`
+        position: relative;
+      `}
+    >
+      <div
+        css={css`
+          display: flex;
+          position: absolute;
+          top: 0;
+          left: 0;
+          width: 100%;
+          height: 100%;
+          justify-content: center;
+          align-items: center;
+        `}
+      >
+        <img
+          src="images/obake.png"
+          alt="logo"
+          css={css`
+            width: 300px;
+            height: 300px;
+          `}
+        />
+        <h1
+          css={(theme) => css`
+            color: ${theme.colors.white};
+            font-size: 60px;
+          `}
+        >
+          nola&apos;s portfolio
+        </h1>
+      </div>
+    </HeroImage>
   );
 };
 
