60087b1cc84af3918a2122260bc2eeadd4111cb4
no-la <nzyc0216@icloud.com>
2024-10-07 00:32:14 +0900

    modify skill-item border

--- src/app/my-skills/skill-item.jsx
+++ src/app/my-skills/skill-item.jsx
@@ -4,7 +4,7 @@ import { css } from '@emotion/react';
 
 const divStyle = (theme) => css`
   text-align: center;
-  border-bottom: 1px solid ${theme.colors.secondary};
+  border-bottom: 1px solid ${theme.colors.background};
 `;
 
 const SkillItem = ({ name, duration }) => {
