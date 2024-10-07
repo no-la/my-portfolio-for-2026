e32c52ce91ad9e44069dea7c2d2a3d77238746e7
no-la <nzyc0216@icloud.com>
2024-10-08 00:04:10 +0900

    add datas

--- src/app/my-concept/my-concept.jsx
+++ src/app/my-concept/my-concept.jsx
@@ -4,15 +4,33 @@ import { css } from '@emotion/react';
 import SectionHeader from '../../components/section-header/section-header';
 
 const wrapStyle = (theme) => css`
-  font-size: ${theme.fontSizes.large};
+  display: flex;
+  align-items: center;
+  gap: 60px;
+  flex-direction: column;
   padding: ${theme.var.sectionPadding};
+`;
+
+const conceptTextStyle = (theme) => css`
+  font-size: ${theme.fontSizes.large};
 `;
 
 const MyConcept = () => {
   return (
     <section>
       <SectionHeader>Concept</SectionHeader>
-      <div css={wrapStyle}>...</div>
+      <div css={wrapStyle}>
+        <div css={conceptTextStyle}>
+          「まっすぐに自分を伝えるポートフォリオ」
+        </div>
+        <div>
+          <p>
+            このサイトは「まっすぐに自分を伝えるポートフォリオ」をコンセプトに制作したポートフォリオサイトです。
+            私のこれまでの取り組みについて、動機やそこから学んだことなどを含めて、詳細に紹介しています。
+            このサイトを通じて、私ができること、好きなこと、大事にしたいことを少しでも知っていただけたらとても嬉しいです！
+          </p>
+        </div>
+      </div>
     </section>
   );
 };
